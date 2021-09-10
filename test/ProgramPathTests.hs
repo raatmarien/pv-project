b8c110416620cce5b7ecde69a6b72f6182860587
Marien <mail@marienraat.nl>
2021-09-10 11:53:41 +0200

    Test empty paths

--- test/ProgramPathTests.hs
+++ test/ProgramPathTests.hs
@@ -6,7 +6,27 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck as QC
 
+instance Arbitrary Stmt where
+  arbitrary = QC.oneof [
+    return Skip,
+    Assert <$> arbitrary,
+    Assume <$> arbitrary,
+    Assign <$> arbitrary <*> arbitrary,
+    Seq <$> arbitrary <*> arbitrary,
+    IfThenElse <$> arbitrary <*> arbitrary <*> arbitrary,
+    While <$> arbitrary <*> arbitrary,
+    Block [] <$> arbitrary]
+
+instance Arbitrary Expr where
+  -- We don't need complicated expressions for these tests
+  arbitrary = return $ LitB True
+
 testSkipPaths = QC.testProperty "test that skip creates valid paths"
                 $ \k -> generateProgramPaths k Skip == [[]]
 
-programPathTests = testGroup "Program path tests" [testSkipPaths]
+testZeroK = QC.testProperty "test that k=0 always returns no paths if the statement is not Skip"
+            $ \stmt -> stmt /= Skip
+                       QC.==> (generateProgramPaths 0 stmt == [])
+
+
+programPathTests = testGroup "Program path tests" [testSkipPaths, testZeroK]
