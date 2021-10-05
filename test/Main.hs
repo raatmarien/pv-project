01a5389197e965b60e5d2c18a954db4a0ad21a90
Mark <reemdvm@gmail.com>
2021-10-05 21:09:51 +0200

    The possibility to use existing examples for testing purposes
    This feature is not completely finished yet, but it is a beginning where I worked on with Philipp in the class.

--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,23 @@
 import ProgramPathTests
 
 import Test.Hspec
+import Text.RawString.QQ (r)
 
 main :: IO ()
 main = hspec $ do
   describe "Tests" $ do
     programPathTests
+    it "external" $ do
+      program <- readFile "examples/E.gcl"
+      program `shouldBe` "hi"
+    it "inline" $
+      [r|
+// Just a very simple example; it is faulty
+E(x:int | y:int){
+  assume 1<x ;
+  while 0<x do{ x:=x-1 } ; y:=x ;
+  assert y=1 // faulty
+}
+      |]
+      `shouldBe`
+      "hi"
